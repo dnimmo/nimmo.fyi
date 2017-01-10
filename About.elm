f875792d88edb58fc92c0114f655a1aadff6f357
David Nimmo <dnimmo@gmail.com>
2017-01-10 20:37:17 +0000

    added About section

---
+++ About.elm
@@ -0,0 +1,12 @@
+module About exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+view =
+    section []
+        [ h2 [] [ text "A little about me" ]
+        , p [] [ text "I'm a software developer, based in Newcastle Upon Tyne, England. I have a great passion for functional programming, and device-agnostic development. I'm a firm believer in organising code sensibly, in the back-end and front-end being of equal importance, in avoiding side effects where you can, and controlling them where you can't." ]
+        , p [] [ text "I'm a huge fan of Elm, and for the most part I still love JavaScript too; even if it isn't as good, or as much fun. ;) " ]
+        ]
--- Main.elm
+++ Main.elm
@@ -1,8 +1,14 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import About
 import Blogs
 import SideProjects
+
+
+about : Html msg
+about =
+    About.view
 
 
 blogs : Html msg
@@ -19,7 +25,8 @@ main =
     div []
         [ h1 [] [ text "David Nimmo" ]
         , div []
-            [ blogs
+            [ about
+            , blogs
             , projects
             ]
         ]
