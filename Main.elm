bb83a327d4d7511af6a7780d54a2f565462cd93c
David Nimmo <dnimmo@gmail.com>
2017-01-10 20:18:42 +0000

    added side projects

--- Main.elm
+++ Main.elm
@@ -2,14 +2,22 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Blogs
+import SideProjects
 
 
 blogs =
     Blogs.view
 
 
+projects =
+    SideProjects.view
+
+
 main =
     div []
         [ h1 [] [ text "David Nimmo" ]
-        , div [] [ blogs ]
+        , div []
+            [ blogs
+            , projects
+            ]
         ]
---
+++ SideProjects.elm
@@ -0,0 +1,25 @@
+module SideProjects exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+view =
+    section []
+        [ h2 [] [ text "Side Projects" ]
+        , ul []
+            [ li []
+                [ h3 [] [ text "Project Arklay" ]
+                , p [] [ text "A simple text-adventure game that I built at a point when I was dissatisfied with my job, and needed something to help motivate me." ]
+                , p []
+                    [ span [] [ text "Code: " ]
+                    , a [ href "https://github.com/dnimmo/project-arklay" ] [ text "API" ]
+                    , a [ href " https://github.com/dnimmo/project-arklay-client" ] [ text "Client" ]
+                    ]
+                , p []
+                    [ span [] [ text "Site: " ]
+                    , a [ href "http://project-arklay.com" ] [ text "project-arklay.com" ]
+                    ]
+                ]
+            ]
+        ]
