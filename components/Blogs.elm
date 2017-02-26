9f165b313ca8af3eda36f5871b6653ee96cb7000
David Nimmo <dnimmo@gmail.com>
2017-02-26 21:25:26 +0000

    more sensible organisation of blog posts

--- components/Blogs.elm
+++ components/Blogs.elm
@@ -2,6 +2,24 @@ module Blogs exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import BlogContent
+import List
+
+
+renderBlogPost : BlogContent.BlogPost -> Html msg
+renderBlogPost blog =
+    li []
+        [ a [ href blog.url ]
+            [ h3 [ class "ExtraSpace" ]
+                [ text blog.title ]
+            ]
+        , p [ class "PostText" ]
+            [ text blog.origin ]
+        ]
+
+
+blogsList =
+    List.map renderBlogPost BlogContent.blogs
 
 
 view : Html msg
@@ -9,38 +27,5 @@ view =
     article []
         [ h2 [] [ text "Things I've written" ]
         , ul [ class "PostContainer" ]
-            [ li [ class "First" ]
-                [ a [ href "https://dev.to/dnimmo/thinking-of-the-next-developer" ]
-                    [ h3 [ class "ExtraSpace" ]
-                        [ text "Thinking of the next developer" ]
-                    ]
-                , p [ class "PostText" ]
-                    [ text "Dev.to" ]
-                ]
-            , li []
-                [ a [ href "https://dev.to/dnimmo/i-was-wrong-to-dismiss-elm-and-i-think-you-probably-aretoo" ]
-                    [ h3 [ class "ExtraSpace" ]
-                        [ text "I was wrong to dismiss Elm, and I think you probably are too" ]
-                    ]
-                , p [ class "PostText" ]
-                    [ text "Dev.to" ]
-                ]
-            , li []
-                [ a [ href "https://medium.com/@dnimmo/responsive-is-a-responsibility-not-a-choice-8ae5455bf7c3#.qvj9j1644" ]
-                    [ h3 [ class "ExtraSpace" ]
-                        [ text "'Responsive' is a responsibility, not a choice" ]
-                    ]
-                , p [ class "PostText" ]
-                    [ text "Medium" ]
-                ]
-            , li []
-                [ a [ href "https://medium.com/@dnimmo/if-youre-going-to-learn-javascript-learn-javascript-5fb3326fe8f5#.e3y2z7gxg" ]
-                    [ h3 [ class "ExtraSpace" ]
-                        [ text "If you're going to learn JavaScript, learn JavaScript"
-                        ]
-                    ]
-                , p [ class "PostText" ]
-                    [ text "Medium" ]
-                ]
-            ]
+            blogsList
         ]
---
+++ content/BlogContent.elm
@@ -0,0 +1,32 @@
+module BlogContent exposing (..)
+
+import Json.Encode
+import Json.Decode exposing (field)
+
+
+type alias BlogPost =
+    { title : String
+    , url : String
+    , origin : String
+    }
+
+
+blogs : List BlogPost
+blogs =
+    [ { title = "Thinking of the next developer"
+      , url = "https://dev.to/dnimmo/thinking-of-the-next-developer"
+      , origin = "Dev.to"
+      }
+    , { title = "I was wrong to dismiss Elm, and I think you probably are too"
+      , url = "https://dev.to/dnimmo/i-was-wrong-to-dismiss-elm-and-i-think-you-probably-aretoo"
+      , origin = "Dev.to"
+      }
+    , { title = "'Responsive' is a responsibility, not a choice"
+      , url = "https://medium.com/@dnimmo/responsive-is-a-responsibility-not-a-choice-8ae5455bf7c3#.qvj9j1644"
+      , origin = "Medium"
+      }
+    , { title = "If you're going to learn JavaScript, learn JavaScript"
+      , url = "https://medium.com/@dnimmo/if-youre-going-to-learn-javascript-learn-javascript-5fb3326fe8f5#.e3y2z7gxg"
+      , origin = "Medium"
+      }
+    ]
